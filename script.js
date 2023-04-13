56451995b5d168ae5dd7618024b98b9f65009c0c
Willian Carvalho <72278301+Williams26-master@users.noreply.github.com>
2023-04-13 16:16:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let n1 = parseFloat(prompt("Digite um numero:"))
+
+let potencia_2 = Math.pow(n1,2)
+let potencia_3 = Math.pow(n1,3)
+let potencia_4 = Math.pow(n1,4)
+let potencia_5 = Math.pow(n1,5)
+
+document.write(`Potencia 2: ${potencia_2}</br> Potencia 3: ${potencia_3}</br> Potencia 4: ${potencia_4}</br> Potencia 5: ${potencia_5}.`)
